2d6edcec79f7254e42b8d3ecf13320b8340a5bd7
zainaw123 <123004710+zainaw123@users.noreply.github.com>
2023-09-26 23:58:44 -0400

    created Window object

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
         System.out.println("game building in process...");
     }
 }
