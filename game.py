22c937fe613dbfc8d6ffb52459bb30943d25fe9c
zainaw123 <123004710+zainaw123@users.noreply.github.com>
2023-12-13 12:15:45 -0500

    added more methods to fit proposal

--- game.py
+++ game.py
@@ -2,28 +2,51 @@ import discord
 import os
 import random
 
-
 intents = discord.Intents.default()
-intents.typing = False  # Disable typing events
-intents.presences = False  # Disable presence events
+intents.typing = False
+intents.presences = False
 
-#connection to discord
 client = discord.Client(intents=intents)
-#register an event
+
 @client.event
-
 async def on_ready():
-
-  print('login successful as {0.user}'.format(client))  
+    print(f'Logged in as {client.user}')
 
 @client.event
 async def on_message(message):
-  if message.content.startswith('/supermario'):
-    print('game running....')  
+    if message.content.startswith('/supermario'):
+        await start_game_session(message)
 
+# function to start game - will implement later
+async def start_game_session(message):
+    """
+    Simulate starting a game session.
+    This will be more complex when integrating with the actual game engine.
+    """
+    print('Game session starting...')
+    await message.channel.send('Welcome to Super Mario Discord! The game is starting...')
+    
+    # Placeholder for multiplayer session initialization
+    # TODO: Implement multiplayer session initiation
 
-#my_secret = os.environ['token']
+    # Simulate day/night cycle
+    if is_night_time():
+        await message.channel.send('It\'s night time in the game. Watch out for different challenges!')
+    else:
+        await message.channel.send('It\'s day time in the game. Enjoy your adventure!')
 
-#client.run(my_secret)
+    # Placeholder for game mechanics
+    # TODO: Implement game mechanics (e.g., moving Mario, jumping, collecting items)
 
+def is_night_time():
+    """
+    Determine if it's night time in the game.
+    This could be based on real-time or a simulated game-time.
+    """
+    # Placeholder for day/night cycle logic
+    # TODO: Implement day/night cycle
+    return random.choice([True, False])
 
+# Uncomment the lines below and set your Discord bot token to run the bot
+# my_secret = os.environ['token']
+# client.run(my_secret)
